0120d5ae268666a1fc05d3dc7bfeb1f4ac0e1f3e
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-09 21:09:12 -0600

    create Reviews controller

---
+++ Backend/app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,40 @@
+module Api
+  module V1
+    class ReviewsController < ApplicationController
+      protect_from_forgery with: :null_session
+
+      def index
+        reviews = Review.all
+
+        render json: ReviewSerializer.new(reviews, options).as_json
+      end
+
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer.new(review).as_json
+        else
+          render json: { error: review.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        review = Review.find(params[:id])
+
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: reivew.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :desc, :score, :shop_id, :user_id)
+      end
+
+    end
+  end
+end
